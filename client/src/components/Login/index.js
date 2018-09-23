988b4a7157951aa57073bc41b319e81b9c649f1b
ilya1995 <kovalyov.king@yandex.ru>
2018-09-23 20:56:46 +0300

    form login 2

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            comand: true
+        };
     }
 
     entry(e) {
@@ -12,18 +15,43 @@ export default class Home extends Component {
         // this.setState({windowHeight: document.documentElement.clientHeight});
     }
 
+    regOrAunt() {
+        let title = document.getElementById('title-form');
+        let form = document.getElementById('form');
+        title.classList.add('is-open');
+        this.setState({comand: !this.state.comand});
+        this.state.comand ? form.classList.add('registrarion') : form.classList.remove('registrarion');
+        setTimeout(()=>{title.classList.remove('is-open')},1000)
+    }
+
+    registration(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <div style={{marginBottom: '1000px'}}>
+            <div style={{marginBottom: '600px'}}>
                 <div className='overlay'>
-                    <form className='form-aunt'>
-                        <div className='title-form'>Авторизация</div>
+                    <form id='form' className='form-aunt'>
+                        <div id='title-form' className='title-form loading-button'>
+                            <Link to='/'  className='btn-close' title='Закрыть форму заказа'/>
+                            {this.state.comand ? 'Авторизация' : 'Регистрация'}
+                        </div>
                         <div className='body-form'>
                             <input type='text' placeholder='Логин'/>
+                            {!this.state.comand ? <input type='email' placeholder='email'/>: null}
                             <input type='password' placeholder='Пароль'/>
+                            {!this.state.comand ? <input type='password' placeholder='Повторите пароль'/>: null}
                             <p className="login-submit">
                                 <button onClick={this.entry} className="login-button">Войти</button>
                             </p>
+                            <div className='text-form'>
+                                <div>
+                                    <div onClick={this.regOrAunt.bind(this)} className='text'>{this.state.comand ? 'Регистрация' : 'Авторизация'}</div>
+                                    <div className='text'>Забыли пароль?</div>
+                                </div>
+                                {!this.state.comand ? <button onClick={this.registration.bind(this)} className='reg-btn'>Подтвердить</button> : null}
+                            </div>
                         </div>
                     </form>
                 </div>
--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import './styles.css'
 
 export default class NotFound extends Component {
     render() {
         return (
-            <div className='row'>
-                <div className='col-md-12'>
-                    Страница не найдена. Вернуться на <Link to='/'>главную</Link>?
+            <div className='not-found'>
+                <div  className='return'>Страница не найдена. Вернуться на <Link to='/'>главную</Link>?</div>
+                <div>
+                    <p id="error">E<span>r</span>ror</p>
+                    <p id="code">4<span>0</span><span>4</span></p>
                 </div>
             </div>
         )
